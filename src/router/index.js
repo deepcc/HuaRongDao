af8ee9f8fd54770533aa94fa717422c01ce70e43
deepcc <58009015@qq.com>
2018-04-13 09:15:09 +0800

    1.0.5

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,12 @@
  * @Author: link 
  * @Date: 2017-11-21 09:54:36 
  * @Last Modified by: link
- * @Last Modified time: 2017-11-27 13:16:44
+ * @Last Modified time: 2018-04-13 09:14:29
  */
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import hrd from '@/components/hrd'
-import dragDrop from '@/components/hrd-drag-drop'
 
 Vue.use(Router)
 
@@ -17,9 +16,5 @@ export default new Router({
         path: '/',
         name: 'hrd',
         component: hrd
-    }, {
-        path: '/dragDrop',
-        name: 'dragDrop',
-        component: dragDrop
     }]
 })
